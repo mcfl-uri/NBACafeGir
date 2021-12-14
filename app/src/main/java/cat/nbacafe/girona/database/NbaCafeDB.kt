2a84e62d46fa95bda541b97f1ef73e1544c6b752
mcfl-uri <lamelasoriol@gmail.com>
2021-12-14 17:39:58 +0100

    dbCreated

--- app/src/main/java/cat/nbacafe/girona/database/NbaCafeDB.kt
+++ app/src/main/java/cat/nbacafe/girona/database/NbaCafeDB.kt
@@ -1,11 +1,17 @@
 package cat.nbacafe.girona.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import cat.nbacafe.girona.database.daos.*
 import cat.nbacafe.girona.database.entities.*
 
-@Database(entities = [Beguda::class, Comanda::class, Postre::class, Sandwich::class, Usuari::class], version = 1, exportSchema = false)
+@Database(
+    entities = [Beguda::class, Comanda::class, Postre::class, Sandwich::class, Usuari::class],
+    version = 1,
+    exportSchema = false
+)
 abstract class NbaCafeDB : RoomDatabase() {
 
     abstract val begudaDao: BegudaDao
@@ -14,5 +20,28 @@ abstract class NbaCafeDB : RoomDatabase() {
     abstract val sandwichDao: SandwichDao
     abstract val usuariDao: UsuariDao
 
+    companion object {
 
+        @Volatile
+        private var INSTANCE: NbaCafeDB? = null
+
+        fun getInstance(context: Context): NbaCafeDB {
+            synchronized(this) {
+                var instance = INSTANCE
+
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        NbaCafeDB::class.java,
+                        "sleep_history_database"
+                    )
+                        .fallbackToDestructiveMigration()
+                        .allowMainThreadQueries()
+                        .build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
 }
