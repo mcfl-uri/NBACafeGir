722274418a8a2517a2fd76e09b93ea047461b43a
mcfl-uri <lamelasoriol@gmail.com>
2021-12-18 19:33:07 +0100

    registre

--- app/src/main/java/cat/nbacafe/girona/ui/fragments/user/login/LoginFragment.kt
+++ app/src/main/java/cat/nbacafe/girona/ui/fragments/user/login/LoginFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import cat.nbacafe.girona.R
@@ -41,8 +42,17 @@ class LoginFragment : Fragment() {
         }
 
         binding.endavantButton.setOnClickListener { View ->
-
-            view?.findNavController()?.navigate(R.id.action_loginFragment_to_homeFragment)
+            if (userViewModel.userExists(binding.loginUser.text.toString())) {
+                if (userViewModel.checkLogin(binding.loginUser.text.toString(), binding.loginPassword.text.toString())) {
+                    view?.findNavController()?.navigate(R.id.action_loginFragment_to_homeFragment)
+                } else {
+                    Toast.makeText(context, "Contrasenya incorrecte", Toast.LENGTH_LONG)
+                        .show()
+                }
+            } else {
+                Toast.makeText(context, "Usuari inexistent", Toast.LENGTH_LONG)
+                    .show()
+            }
         }
 
         return binding.root
