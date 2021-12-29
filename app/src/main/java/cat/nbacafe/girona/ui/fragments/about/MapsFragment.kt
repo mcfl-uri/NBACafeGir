e4f162068d307b1d172525070bb26bfe64460177
mcfl-uri <lamelasoriol@gmail.com>
2021-12-29 19:52:00 +0100

    mapa funciona falta tornar enrere

---
+++ app/src/main/java/cat/nbacafe/girona/ui/fragments/about/MapsFragment.kt
@@ -0,0 +1,47 @@
+package cat.nbacafe.girona.ui.fragments.about
+
+import androidx.fragment.app.Fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import cat.nbacafe.girona.R
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MapsFragment : Fragment() {
+
+    private val callback = OnMapReadyCallback { googleMap ->
+        /**
+         * Manipulates the map once available.
+         * This callback is triggered when the map is ready to be used.
+         * This is where we can add markers or lines, add listeners or move the camera.
+         * In this case, we just add a marker near Sydney, Australia.
+         * If Google Play services is not installed on the device, the user will be prompted to
+         * install it inside the SupportMapFragment. This method will only be triggered once the
+         * user has installed Google Play services and returned to the app.
+         */
+        val cafeteria = LatLng(41.99274255451547, 2.812562295815218)
+        googleMap.addMarker(MarkerOptions().position(cafeteria).title("NBA Café Girona"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(cafeteria, 19.0F))
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_maps, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(callback)
+    }
+}
