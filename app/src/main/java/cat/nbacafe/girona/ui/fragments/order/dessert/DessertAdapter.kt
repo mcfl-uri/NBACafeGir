e6969138a37c1ae320f803f967717d45a5823699
mcfl-uri <lamelasoriol@gmail.com>
2022-01-05 22:17:47 +0100

    prova fav dessert

--- app/src/main/java/cat/nbacafe/girona/ui/fragments/order/dessert/DessertAdapter.kt
+++ app/src/main/java/cat/nbacafe/girona/ui/fragments/order/dessert/DessertAdapter.kt
@@ -3,6 +3,7 @@ package cat.nbacafe.girona.ui.fragments.order.dessert
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import cat.nbacafe.girona.R
@@ -11,14 +12,25 @@ import cat.nbacafe.girona.database.entities.Sandwich
 import cat.nbacafe.girona.ui.fragments.order.sandwiches.SandwichAdapter
 
 class DessertAdapter(
-    val dessert: List<Postre>,
-    private val clickListener: (Postre) -> Unit
+    val dessert: List<Postre>
 ) :
     RecyclerView.Adapter<DessertAdapter.DessertHolder>() {
 
+    lateinit var mListener: onItemClickListener
+
+    interface onItemClickListener {
+        fun onItemClick(position: Int)
+        fun onAddFavClick(position: Int)
+        fun onRemFavClick(position: Int)
+    }
+
+    fun setOnItemClickListener(listener: onItemClickListener) {
+        mListener = listener
+    }
+
     override fun getItemCount() = dessert.size
 
-    class DessertHolder(val view: View, clickPosition: (Int) -> Unit) :
+    class DessertHolder(val view: View, listener: onItemClickListener) :
         RecyclerView.ViewHolder(view) {
         fun bind(dessert: Postre) {
             view.findViewById<TextView>(R.id.dessertNom).text = dessert.nomPostre
@@ -29,21 +41,25 @@ class DessertAdapter(
 
         init {
             itemView.setOnClickListener {
-                clickPosition(adapterPosition)
+                listener.onItemClick(adapterPosition)
+            }
+            val addFav: ImageView = view.findViewById(R.id.nonFavSandwich)
+            addFav.setOnClickListener {
+                listener.onAddFavClick(adapterPosition)
+            }
+            val remFav: ImageView = view.findViewById(R.id.favSandwich)
+            remFav.setOnClickListener {
+                listener.onRemFavClick(adapterPosition)
             }
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DessertHolder {
 
-        val dh = DessertHolder(
-            LayoutInflater.from(parent.context)
+        val dh = LayoutInflater.from(parent.context)
                 .inflate(R.layout.dessert_cell_layout, parent, false)
-        ) {
-            clickListener(dessert[it])
-        }
 
-        return dh
+        return DessertHolder(dh, mListener)
     }
 
     override fun onBindViewHolder(holder: DessertHolder, position: Int) {
--- app/src/main/java/cat/nbacafe/girona/ui/fragments/order/dessert/DessertFragment.kt
+++ app/src/main/java/cat/nbacafe/girona/ui/fragments/order/dessert/DessertFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
@@ -43,12 +44,28 @@ class DessertFragment : Fragment() {
         binding.dessertRecycler.layoutManager =
             LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
 
-        val adapter = DessertAdapter(dessert) {
-            sharedViewModel.addCourse(it.nomPostre, it.preuPostre, 1)
-            view?.findNavController()?.navigate(R.id.action_dessertFragment_to_drinkFragment)
-        }
+        val adapter = DessertAdapter(dessert)
 
         binding.dessertRecycler.adapter = adapter
+
+        val nonFav = view?.findViewById<ImageView>(R.id.nonFavSandwich)
+        val fav = view?.findViewById<ImageView>(R.id.favSandwich)
+
+        adapter.setOnItemClickListener(object : DessertAdapter.onItemClickListener {
+            override fun onItemClick(position: Int) {
+                sharedViewModel.addCourse(dessert[position].nomPostre, dessert[position].preuPostre, 1)
+                view?.findNavController()?.navigate(R.id.action_dessertFragment_to_drinkFragment)
+            }
+
+            override fun onAddFavClick(position: Int) {
+                fav
+            }
+
+            override fun onRemFavClick(position: Int) {
+                TODO("Not yet implemented")
+            }
+
+        })
 
         binding.setLifecycleOwner(this)
 
